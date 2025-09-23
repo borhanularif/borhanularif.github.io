2c695d2f1fe8d263864b641ecbb23f5ffdf6b713
borhanularif <63269814+borhanularif@users.noreply.github.com>
2025-09-23 10:33:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+} 
+
+$(document).ready(function(){
+
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Java Developer", "App Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+window.onscroll = function() {
+    var scrollUpBtn = document.getElementById('scroll-up-btn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollUpBtn.style.display = 'block';
+    } 
+    
+    else {
+        scrollUpBtn.style.display = 'none';
+    }
+};
+
+function sendEmail(){  
+    var name=document.getElementById("name").value;  
+    var email=document.getElementById("email").value;  
+    var subject=document.getElementById("subject").value;  
+    var body=document.getElementById("body").value;
+    window.open('mailto:abusaeed2433@gmail.com?'+'&subject='+subject+'&body='+body);  
+}
+
+//showPublications("All");
+showProjects('Allp');
+
+function showPublications(year) {
+    var allContent = document.querySelectorAll("#publicationContent > ul");
+
+    var buttons = document.querySelectorAll('.button-publications');
+    buttons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+
+    if (year === "All") {
+        allContent.forEach(function(content) {
+            content.style.display = "block";
+        });
+        document.getElementById("btnAll").classList.add('selected');
+    } else {
+        allContent.forEach(function(content) {
+            if (content.id === "year" + year) {
+                content.style.display = "block";
+                document.getElementById("btn" + year).classList.add('selected');
+            } else {
+                content.style.display = "none";
+            }
+        });
+    }
+}
+
+
+function showProjects(topic) {
+    var allContent = document.querySelectorAll("#projectContent > ul");
+
+    var buttons = document.querySelectorAll('.button-projects');
+    buttons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+
+    if (topic === "Allp") {
+        allContent.forEach(function(content) {
+            content.style.display = "block";
+        });
+        document.getElementById("btnAllp").classList.add('selected');
+    } else {
+        allContent.forEach(function(content) {
+            if (content.id === "pr" + topic) {
+                content.style.display = "block";
+                document.getElementById("btn" + topic).classList.add('selected'); 
+            }
+            
+            else if (content.id === "prBoth") {
+                content.style.display = "block";
+                document.getElementById("btn" + topic).classList.add('selected'); 
+            }
+            
+            else {
+                content.style.display = "none";
+            }
+        });
+    }
+}
